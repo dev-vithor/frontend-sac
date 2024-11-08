4c8651f3c45ba84dd4fc424b088122e68fb79b2d
dev-vithor <vitinhobrt0@gmail.com>
2024-11-08 15:36:30 -0300

    :rocket: Confimações antes do deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,7 +6,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <App />,
 )
